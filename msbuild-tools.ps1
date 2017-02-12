19a0054a683a8be691172984a668cbf349233950
= <=>
2017-02-12 12:01:11 -0800

    updated to use git and msbuild

--- msbuild-tools.ps1
+++ msbuild-tools.ps1
@@ -1,48 +1,44 @@
-function update-dependency($repo = "C:\Users\dustinf\Source\Repos", $lib = "DepLib", $ver="1.0.5", $targetBranch="Feature2"){
-    push-location $repo
-	$filter = "packages.config";
-    write-host "searching all packages.config in $repo for $lib"
-	if($lib -eq ""){
-		write-host "please provide a lib value"
-	}
-	
-    $files = gci $repo -filter $filter -recurse
-    $files | % {
-        $content = gc $_.FullName | select-string -Pattern $lib
-        if($content -and $content -ne ""){
-           $content = $content.ToString();
-           if($content.IndexOf("-") -eq -1){ ## TODO: handle semver
-                $temp = $content.SubString($content.IndexOf("version") + 9, $ver.Length);
-                $oldVer = [int]($temp.Replace(".",""));
-                $newVer = [int]($ver.Replace(".",""));
-                if($newVer -gt $oldVer){
-                    write-host -ForegroundColor Red "Update Found";
-                    write-host "---------------";
-                    write-host $_.FullName;
-                    write-host "Old: $temp New: $ver"
-                    $toUpdate = read-host "Update? [y]/n";
-                    if($toUpdate -ieq "y" -or $toUpdate -eq ""){
-                        ## first update the file contents
-                        $fc = gc $_.FullName;
-                        $newcontent = $content.Replace($temp, $ver);
-                        $fc = $fc.Replace($content, $newcontent);
-                        set-content -path $_.FullName -value $fc;
-                        ## restore new package
-                        Push-Location $_.Directory.FullName
-                        $nuget = "F:\Utility\nuget.exe"
-                        & nuget restore
-
-                        ## build
-                        ## https://blog.corsamore.com/2013/08/31/drop-msbuild-have-a-glass-of-psake/
-                        ## https://github.com/deadlydog/Invoke-MsBuild
-                        Invoke-Psake -framework 4.0 .\build.ps1 $target
-
-                        write-host -ForegroundColor Green "Updated";
-                    }
-                }
-            }
+function update-reference{
+    param(
+        [Parameter(Mandatory=1)]
+        [string]$repo,
+        [Parameter(Mandatory=1)]
+        [string]$lib,
+        [Parameter(Mandatory=1)]
+        [string]$ver,
+        [Parameter(Mandatory=1)]
+        [string]$branch,
+        [Parameter(Mandatory=1)]
+        [string]$nugetPath
+    )
+    ## uses %AppData%\NuGet\NuGet.config
+    ## depends on https://github.com/deadlydog/Invoke-MsBuild for msbuild
+    ## https://www.powershellgallery.com/packages/Invoke-MsBuild/2.2.0
+    ## C:\Program Files\WindowsPowerShell\Modules\Invoke-MsBuild\2.2.0
+    
+    push-location $repo;
+    $files = ls *.sln -recurse;
+    $files | % {        
+        push-location $_.DirectoryName;
+        $sln = $_.FullName;
+        git checkout $branch
+        & $nugetPath restore $sln
+        $nugetResult = & $nugetPath update $sln -id $lib -version $ver
+        if($nugetResult -like "*No projects found*"){
+            continue;
         }
+
+        $buildResult = invoke-msbuild -path $sln
+        if($buildResult.BuildSucceeded -eq $true){
+            git commit -m "update packages"
+            #git push
+        }else{
+             gc $buildResult.BuildErrorsLogFilePath | write-host -foregroundcolor RED
+        }       
     }
 }
 
-update-dependency
+$nuget = "C:\Utilities\NuGet.exe";
+update-reference -repo "C:\Work\Repo" -lib "deplib" -ver "1.0.5" -branch "feature2" -nugetPath $nuget
+
+
